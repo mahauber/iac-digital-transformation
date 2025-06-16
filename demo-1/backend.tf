3ff2778ed4bfaccfa9fed0638e0bcfa1c60387f3
Matthias Hauber <matthias.hauber@prodyna.com>
2025-06-16 20:56:06 +0200

    Add backend configuration files for demo environments

---
+++ demo-1/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    use_azuread_auth     = true                     # Can also be set via `ARM_USE_AZUREAD` environment variable.
+    storage_account_name = "saseed"               # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate-github-demo"    # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "demo-1.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
